81515421c7cfc6547a97e58666d8366e3c944c2b
aboorde <andrew.boorde@gmail.com>
2016-05-22 09:44:35 -0600

    Added tone analysis

--- talkToMe.py
+++ talkToMe.py
@@ -5,6 +5,11 @@ import monotonic
 # github/uberi/speech_recognition
 import speech_recognition as sr
 
+# bring in tone analyzer
+import json
+from watson_developer_cloud import ToneAnalyzerV3Beta
+
+
 # get audio from mic
 r = sr.Recognizer()
 with sr.Microphone() as source:
@@ -16,8 +21,18 @@ IBM_USERNAME = ""
 IBM_PASSWORD = ""
 
 try:
-	print("Watson thinks you said: " + r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD))
+	audOut = r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD)
+	print("Watson thinks you said: " + audOut)
 except sr.UnknownValueError:
 	print("WHAT DID YOU SAY!? - Watson")
 except sr.RequestError as e:
 	print("WATSONS DOWN OR SOMETHING; {0}".format(e))
+
+
+TONE_USERNAME = ""
+TONE_PASSWORD = ""
+TONE_VERSION = ""
+
+tone_analysis = ToneAnalyzerV3Beta(username=TONE_USERNAME, password=TONE_PASSWORD, version=TONE_VERSION)
+
+print(json.dumps(tone_analysis.tone(text=audOut), indent=2))
